4b865e3a6a41340df3e3a3aeeb356751756a5f3f
thiagobarufaldi <39417670+thiagobarufaldi@users.noreply.github.com>
2024-10-27 17:59:44 -0300

    Markdown to blocks function and tests finished

--- src/markdown_block.py
+++ src/markdown_block.py
@@ -1,4 +1,9 @@
 def markdown_to_blocks(markdown: str):
-    if len(markdown) > 0:
-        blocks = markdown.split('\n\n')
-        return blocks
+    if not markdown:
+        return []
+
+    blocks = markdown.split('\n\n')
+    clean_blocks = [block.strip() for block in blocks]
+    complete_blocks = list(filter(None, clean_blocks))
+
+    return complete_blocks
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -6,9 +6,37 @@ class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
         test_input = """#Heading
 
-        Paragraph text
+                        Paragraph text
 
-        * List item"""
+                        * List item"""
+        result = markdown_to_blocks(test_input)
+        expected_output = ["#Heading", "Paragraph text", "* List item"]
+        self.assertEqual(result, expected_output)
+
+    def test_markdown_empty(self):
+        test_input = ""
+        result = markdown_to_blocks(test_input)
+        expected_output = []
+        self.assertEqual(result, expected_output)
+
+    def test_markdown_spaces(self):
+        test_input = """#Heading
+
+                        Paragraph text
+
+                        * List item"""
+        result = markdown_to_blocks(test_input)
+        expected_output = ["#Heading", "Paragraph text", "* List item"]
+        self.assertEqual(result, expected_output)
+
+    def test_markdown_multiple_blanks(self):
+        test_input = """#Heading
+
+
+                        Paragraph text
+
+
+                        * List item"""
         result = markdown_to_blocks(test_input)
         expected_output = ["#Heading", "Paragraph text", "* List item"]
         self.assertEqual(result, expected_output)
